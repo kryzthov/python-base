ee9c4f4a4ff3689463e0f5beb82f9740976c80df
Christophe Taton <taton@wibidata.com>
2014-05-30 11:20:33 -0700

    Update to new development version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def Main():
 
   setup(
     name = 'python-base',
-    version = '1.0.9',
+    version = '1.0.10-SNAPSHOT',
     packages = ['base', 'workflow'],
     package_dir = {
         'base': 'base',
