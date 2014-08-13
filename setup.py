86bf69c13716d8841fc4f07bc0196ec4fe10819e
Christophe Taton <taton@wibidata.com>
2014-08-13 08:50:43 -0700

    New development branch 1.0.11-SNAPSHOT

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def Main():
 
   setup(
     name = 'python-base',
-    version = '1.0.10',
+    version = '1.0.11-SNAPSHOT',
     packages = ['base', 'workflow'],
     package_dir = {
         'base': 'base',
